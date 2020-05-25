f0e20ea35e147585dfc3c0a7b46327c41f34a6ff
Crowdbotics <team@crowdbotics.com>
2020-05-25 23:38:12 +0000

    Update models

--- lizz_5_26_demo_empt_17390/wsgi.py
+++ lizz_5_26_demo_empt_17390/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_5_26_demo_empt_17390.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lizz_5_26_demo_empt_17390.settings")
 
 application = get_wsgi_application()
